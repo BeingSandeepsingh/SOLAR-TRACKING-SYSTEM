d40eb458d5b44e99796d6a967bd84b65482d05a9
sandeep_0405 <94966432+BeingSandeepsingh@users.noreply.github.com>
2023-07-28 15:00:33 +0530

    Add files via upload

---
+++ solar tracker project/testarduino/testarduino.ino
@@ -0,0 +1,17 @@
+int led=13;
+void setup() {
+pinMode(led,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+digitalWrite(led,HIGH);
+delay(5000);
+digitalWrite(led,LOW);
+delay(5000);
+
+
+  // put your main code here, to run repeatedly:
+
+}
